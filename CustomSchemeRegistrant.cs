1b9a3a46e5000a37dad679c7a930d0e7879d8d59
LemonNeko <self@lemonneko.moe>
2024-01-02 17:56:31 +0800

    support linux

--- CustomSchemeRegistrant.cs
+++ CustomSchemeRegistrant.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.IO;
 using Godot;
 using Microsoft.Win32;
 
@@ -9,20 +11,42 @@ public partial class CustomSchemeRegistrant : Node
     public override void _Ready()
     {
         GD.Print("[CustomSchemeRegistrant] Registering custom scheme...");
+        var currentExecutablePath = OS.GetExecutablePath();
+        GD.Print($"[CustomSchemeRegistrant] Current executable path: {currentExecutablePath}");
         GD.Print($"[CustomSchemeRegistrant] OS: {OS.GetName()}");
 
         if (OperatingSystem.IsWindows())
         {
-            var currentExecutablePath = OS.GetExecutablePath().Replace('/', '\\');
-            GD.Print($"[CustomSchemeRegistrant] Current executable path: {currentExecutablePath}");
+            var currentExecutablePathForWindows = OS.GetExecutablePath().Replace('/', '\\');
+            GD.Print($"[CustomSchemeRegistrant] Current executable path: {currentExecutablePathForWindows}");
 
             var protocolKey = Registry.CurrentUser.CreateSubKey(@"Software\Classes\godot-scheme-test");
             protocolKey!.SetValue("URL Protocol", "", RegistryValueKind.String);
 
             var commandKey =
                 Registry.CurrentUser.CreateSubKey(@"Software\Classes\godot-scheme-test\shell\open\command");
-            commandKey!.SetValue("", $"{currentExecutablePath} %1", RegistryValueKind.String);
+            commandKey!.SetValue("", $"{currentExecutablePathForWindows} %1", RegistryValueKind.String);
 
+            GD.Print($"[CustomSchemeRegistrant] Done.");
+        }
+        else if (OperatingSystem.IsLinux())
+        {
+            File.WriteAllText("~/.local/share/applications/godot-scheme-test.desktop", $"""
+                                                                                       [Desktop Entry]
+                                                                                       Type=Application
+                                                                                       Name=CustomSchemePracticeForGodot
+                                                                                       Exec={currentExecutablePath} %u
+                                                                                       MimeType=x-scheme-handler/godot-scheme-test
+                                                                                       Terminal=false
+                                                                                       """);
+            var setDefaultProcessInfo = new ProcessStartInfo("xdg-mime", ["default", "~/.local/share/applications/godot-scheme-test.desktop", "x-scheme-handler/godot-scheme-test"]);
+            var setDefaultProcess = Process.Start(setDefaultProcessInfo);
+            setDefaultProcess!.WaitForExit();
+            if (setDefaultProcess.ExitCode != 0)
+            {
+                GD.Print($"[CustomSchemeRegistrant] Failed.");
+                return;
+            }
             GD.Print($"[CustomSchemeRegistrant] Done.");
         }
         else
